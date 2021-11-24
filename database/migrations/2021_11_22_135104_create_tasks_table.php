5de6bf628fe82ccce629eb880048738311cdb860
QVictor <QQlogin@yandex.ru>
2021-11-24 14:19:51 +0300

    migration create table tasks

---
+++ database/migrations/2021_11_22_135104_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('author');
+            $table->dateTime('deadline');
+            $table->timestamps();
+        });
+
+        (new \Database\Seeders\TaskSeeder())->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
