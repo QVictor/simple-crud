d78912b3df73fa015fe30cfa36e111cc4660233f
QVictor <QQlogin@yandex.ru>
2021-11-24 14:26:42 +0300

    add seeder for tasks

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(100),
+            'author' => $this->faker->name(),
+            'deadline' => $this->faker->dateTimeBetween(
+                Carbon::today()->format('Y-m-d H:m:s'),
+                Carbon::today()->addMonths(6)->format('Y-m-d H:m:s'))
+        ];
+    }
+}
